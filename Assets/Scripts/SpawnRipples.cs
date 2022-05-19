599aadf55fcc42b19fd61a9c239b51a13f186980
Chris Dayton <chrisdayt@gmail.com>
2022-05-19 17:12:29 -0500

    Made a black hole, but not entirely happy with it. The colors used leave something to be desired.

--- Assets/Scripts/SpawnRipples.cs
+++ Assets/Scripts/SpawnRipples.cs
@@ -8,15 +8,13 @@ public class SpawnRipples : MonoBehaviour
     public GameObject shieldRipples;
     public Transform shieldTransform;
     
-    private VisualEffect shieldRipplesVfx;
-
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Projectile")
         {
             // instantiate the ripples on the same transform as the shield
             var ripples = Instantiate(shieldRipples, shieldTransform) as GameObject;
-            shieldRipplesVfx = shieldRipples.GetComponent<VisualEffect>();
+            var shieldRipplesVfx = shieldRipples.GetComponent<VisualEffect>();
             shieldRipplesVfx.SetVector3("CollisionCenter", collision.contacts[0].point);
 
             // destroy in short delay
